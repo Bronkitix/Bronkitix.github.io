1660b342bf4448ff50c02def24608dbc556ba762
Bronkitix <yosoyshogi@gmail.com>
2025-05-22 19:23:33 -0400

    1.0

---
+++ main.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x9a9492,
+  color2: 0xff8819,
+  backgroundColor: 0x0,
+  size: 0.50
+  });
+  
